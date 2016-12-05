2da8d04178a36c1b2891e8f8e3a9c19c8597afd0
Adrien Hobbs <adrien.hobbs@gmail.com>
2016-12-05 12:57:01 -0500

    change icon type
    no circle around icon

--- src/components/ui/icons/facebook.js
+++ src/components/ui/icons/facebook.js
@@ -7,11 +7,10 @@ const Facebook = React.createClass({
 
   render () {
     return (
-      <svg ref='icon' width='26px' height='26px' fill={this.props.color} stroke={this.props.color} xmlns='http://www.w3.org/2000/svg' viewBox='0 0 75 75'>
-        <circle cx='37.5' cy='37.5' r='35' fill='none' strokeWidth='5' strokeMiterlimit='10'/>
-        <path
-          d='M49.6 13.3h-6.3c-7.1 0-11.6 4.7-11.6 11.9v5.5h-6.3c-.5 0-1 .4-1 1v8c0 .5.4 1 1 1h6.3v20.1c0 .5.4 1 1 1H41c.5 0 1-.4 1-1V40.6h7.4c.5 0 1-.4 1-1v-8c0-.3-.1-.5-.3-.7s-.4-.3-.7-.3H42V26c0-2.2.5-3.4 3.5-3.4h4.2c.5 0 1-.4 1-1v-7.4c-.1-.5-.6-.9-1.1-.9z'
-          />
+      <svg ref='icon' width='20px' height='20px' fill={this.props.color} stroke={this.props.color} xmlns='http://www.w3.org/2000/svg' viewBox='0 0 50 50'>
+        <path d="M37.982,0.36v7.932h-4.718c-1.723,0-2.885,0.36-3.487,1.08c-0.601,0.72-0.902,1.802-0.902,3.244
+          v5.681h8.804l-1.171,8.895h-7.633V50h-9.194V27.193h-7.663v-8.895h7.663v-6.549c0-3.727,1.042-6.616,3.126-8.669S27.666,0,31.13,0
+          C34.076,0,36.36,0.12,37.982,0.36z"/>
       </svg>
     );
   }
